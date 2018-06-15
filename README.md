ea596a70db51f5459822f7c7a485ba3ee585283a
ivo <ivo@devlabs>
2018-06-15 17:26:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+## crud-route-builder
+This is used to build simple routes for create/read/update/delete operations for your Mongoose models.
+
+### Installation
+``npm install crud-route-builder --save``
+
+### Example
+
+```js
+const express = require('express');
+const router = express.Router();
+const {buildRoutes, Route} = require('crud-route-builder');
+const Example1 = require('../models/Example1');
+const Example2 = require('../models/Example1');
+
+const app = express();
+
+router.use('/example-1', buildRoutes(Example1));
+router.use('/example-2', buildRoutes(Example2,
+    [
+        Route('post', '/custom-route', (req, res, next) => {
+            ...
+        })
+    ]
+));
+
+app.use('/', router);
+```
+
+This example will create the following routes:
+
+``GET /example-1/all `` - Returns all documents. Additional GET params can be sent to filter or sort the request:
+
+    _start  = NUMBER
+    _end    = NUMBER
+    _order  = DESC | ASC    
+    _sort   = FIELD NAME    
+    field   = value
+    
+Example: `GET /example-1/all/?start=0&_end=10_order=ASC&_sort=name&color=red` 
+This request will return the first 10 documents, where the field "color" is "red", 
+sorted by the field "name" in ascending order.
+
+``GET /example-1/single/:id `` - Returns a single document by id.
+
+``POST /example-1/`` - Creates a document. The data is expected to be sent in the body. 
+
+``DELETE /example-1/:id`` - Deletes a document by id.
+
+``PUT /example-1/:id`` - Updates a document. The data is expected to be sent in the body. Only updates the provided fields.
+
+
+###Additional note
+
+This module requires you to use `body-parser`. It won't work without it.
---
+++ index.js
@@ -0,0 +1,199 @@
+const express = require('express');
+const router = express.Router();
+
+const Err = (status, msg) => {
+    let err = new Error();
+    err.status = status;
+    err.message = msg;
+    return err;
+};
+
+/**
+ * Builds all routes for a given resource
+ * The routes generated are:
+ *      GET     /all
+ *      GET     /single/:id
+ *      PUT     /single/:id
+ *      POST    /single/
+ *      DELETE  /single/:id
+ *
+ * @param Resource
+ * @param Extensions: array
+ *
+ *        Enables the declaration of custom routes
+ *        [
+ *          {
+ *              method: "get" || "post" || "put" || "delete",
+ *              route: String,
+ *              function: Function(req, res, next)
+ *          }
+ *        ]
+ *
+ *
+ * @returns {*}
+ */
+
+const buildRoutes = (Resource, Extensions = []) => {
+
+    /**
+     * Custom routes are declared at the top to enable overwriting of the default routes
+     */
+    const allowedMethods = ['get', 'post', 'put', 'delete'];
+    for (let i = 0; i < Extensions.length; i++) {
+        let ex = Extensions[i];
+        if (ex.method && ex.route && ex.function) {
+            if (allowedMethods.indexOf(ex.method) !== -1) {
+                router[ex.method](ex.route, ex.function);
+            }
+        }
+    }
+
+    /**
+     * Returns an array of filtered resources
+     *
+     * Accepted params:
+     *      _end    = NUMBER
+     *      _start  = NUMBER
+     *      _order  = DESC | ASC
+     *      _sort   = FIELD NAME
+     *      field   = value
+     */
+    router.get('/all', (req, res, next) => {
+        try {
+            let filter = {};
+            let modelProps = Object.keys(Resource.schema.paths);
+            let filters = Object.keys(req.query).filter((param) => modelProps.includes(param));
+
+            filters.forEach(f => {
+                let val = req.query[f];
+
+                // Value is a Number
+                if (!isNaN(val)) {
+                    filter[f] = Number(val);
+                }
+
+                // Value is a Bool
+                else if( ["true", "false"].indexOf(val.toLowerCase()) !== -1 ) {
+                    filter[f] = val.toLowerCase() === "true";
+                }
+
+                // Value is a String
+                else {
+                    filter[f] = new RegExp(val, "i");
+                }
+            });
+
+            let query = Resource.find(filter);
+
+            if (req.query._sort && req.query._order) {
+                if (req.query._order === 'ASC') {
+                    query.sort([[req.query._sort, 1]])
+                }
+                else if (req.query._order === 'DESC') {
+                    query.sort([[req.query._sort, -1]])
+                }
+            }
+
+            query.exec()
+                .then((data) => {
+                    res.header("X-Total-Count", data.length);
+
+                    if (req.query._start !== undefined && req.query._end !== undefined) {
+                        data = data.filter((o, i) => i >= req.query._start && i <= req.query._end);
+                    }
+
+                    res.send(data);
+                })
+                .catch(e => next(Err(400, e.message)));
+        }
+        catch (e) {
+            next(e);
+        }
+    });
+
+    /**
+     * Retrieves a single resource object with a given id
+     */
+    router.get('/single/:id', (req, res, next) => {
+        try {
+            Resource.findOne({_id: req.params.id}).exec()
+                .then((data) => {
+                    res.send(data);
+                })
+                .catch(e => next(Err(400, e.message)));
+        }
+        catch (e) {
+            next(e);
+        }
+    });
+
+    /**
+     * Updates a single resource object with the provided fields-value pairs
+     */
+    router.put('/:id', (req, res, next) => {
+        try {
+            Resource.findByIdAndUpdate(req.params.id, {$set: req.body}, {
+                new: true,
+                runValidators: true
+            })
+                .then((data) => {
+                    res.send(data);
+                })
+                .catch(e => next(Err(400, e.message)));
+        }
+        catch (e) {
+            next(e);
+        }
+    });
+
+    /**
+     * Creates a new resource object
+     */
+    router.post('/', (req, res, next) => {
+        try {
+            let resource = new Resource(req.body);
+            resource.save()
+                .then((resource) => {
+                    res.send(resource);
+                })
+                .catch(e => next(Err(400, e.message)));
+        }
+        catch (e) {
+            next(e);
+        }
+    });
+
+    /**
+     * Deletes a resource by id
+     */
+    router.delete('/:id', (req, res, next) => {
+        try {
+            Resource.find({_id: req.params.id}).remove().exec()
+                .then((data) => {
+                    res.send(data);
+                });
+        }
+        catch (e) {
+            next(e);
+        }
+    });
+
+    return router;
+};
+
+/**
+ * Used for creating custom routes or overwriting the default ones
+* @param method : String    - Accepted values are [get, post, put, delete]
+* @param route  : String    
+* @param func   : Function
+ */
+}
+const Route = (method, route, func) => {
+    return {
+        method: method,
+        route: route,
+        function: func
+    }
+};
+
+module.exports = {buildRoutes, Route};
