a6c2dd3156449d6cb1cd8260a14343676c550897
ivo <ivo@devlabs>
2018-06-19 15:43:05 +0300

    added middleware functionality

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const express = require('express');
-const router = express.Router();
 
 const Err = (status, msg) => {
     let err = new Error();
     err.status = status;
     err.message = msg;
     return err;
+};
+
+const isFunction = (functionToCheck) => {
+    return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+};
+
+const allowedMethods = ['get', 'post', 'put', 'delete'];
+
+/**
+ * Used for creating custom routes or overwriting the default ones
+ * @param method : String    - Accepted values are [get, post, put, delete]
+ * @param route  : String
+ * @param func   : Function
+ */
+const Route = (method, route, func) => {
+    return {
+        method: method,
+        route: route,
+        function: func
+    }
+};
+
+const extractMiddleware = (routes, method, route) => {
+    return routes.filter(r => {
+        return r.method === method && r.route === route
+    }) || [];
+};
+
+const validateRoutes = (routes) => {
+    for (let i = 0; i < routes.length; i++) {
+        let r = routes[i];
+        if (
+            allowedMethods.indexOf(r.method) === -1
+            || !(typeof r.route === 'string' || r.route instanceof String)
+            || !isFunction(r.function)
+        ) {
+            throw("Invalid Route object");
+        }
+    }
 };
 
 /**
@@ -18,32 +56,36 @@ const Err = (status, msg) => {
  *      DELETE  /single/:id
  *
  * @param Resource
- * @param Extensions: array
- *
- *        Enables the declaration of custom routes
- *        [
- *          {
- *              method: "get" || "post" || "put" || "delete",
- *              route: String,
- *              function: Function(req, res, next)
- *          }
- *        ]
- *
+ * @param args: {
+ *        extensions: [Array] Enables the declaration of custom routes
+ *                              [
+ *                                  {
+ *                                      method: "get" || "post" || "put" || "delete",
+ *                                      route: String,
+ *                                      function: Function(req, res, next)
+ *                                  }
+ *                              ]
+ *        before: [Array] Middleware that is run before the default functions. Same structure as extensions
+ *        after:  [Array] Middleware that is run after the default functions. Same structure as extensions
  *
  * @returns {*}
  */
 
-const buildRoutes = (Resource, Extensions = []) => {
+const buildRoutes = (Resource, args = {}) => {
+    const router = express.Router();
+
+    let extensions = args.extensions || [];
+    let before = args.before || [];
+    let after = args.after || [];
 
     /**
      * Custom routes are declared at the top to enable overwriting of the default routes
      */
-    const allowedMethods = ['get', 'post', 'put', 'delete'];
-    for (let i = 0; i < Extensions.length; i++) {
-        let ex = Extensions[i];
+    for (let i = 0; i < extensions.length; i++) {
+        let ex = extensions[i];
         if (ex.method && ex.route && ex.function) {
             if (allowedMethods.indexOf(ex.method) !== -1) {
-                router[ex.method](ex.route, ex.function);
+                router[ex.method](ex.route, extractMiddleware(before, ex.method, ex.route), ex.function, extractMiddleware(after, ex.method, ex.route));
             }
         }
     }
@@ -58,7 +100,7 @@ const buildRoutes = (Resource, Extensions = []) => {
      *      _sort   = FIELD NAME
      *      field   = value
      */
-    router.get('/all', (req, res, next) => {
+    router.get('/all', extractMiddleware(before, 'get', '/all'), (req, res, next) => {
         try {
             let filter = {};
             let modelProps = Object.keys(Resource.schema.paths);
@@ -103,34 +145,36 @@ const buildRoutes = (Resource, Extensions = []) => {
                     }
 
                     res.send(data);
+                    next();
                 })
                 .catch(e => next(Err(400, e.message)));
         }
         catch (e) {
             next(e);
         }
-    });
+    }, extractMiddleware(after, 'get', '/all'));
 
     /**
      * Retrieves a single resource object with a given id
      */
-    router.get('/single/:id', (req, res, next) => {
+    router.get('/single/:id', extractMiddleware(before, 'get', '/single/:id'), (req, res, next) => {
         try {
             Resource.findOne({_id: req.params.id}).exec()
                 .then((data) => {
                     res.send(data);
+                    next();
                 })
                 .catch(e => next(Err(400, e.message)));
         }
         catch (e) {
             next(e);
         }
-    });
+    }, extractMiddleware(after, 'get', '/single/:id'));
 
     /**
      * Updates a single resource object with the provided fields-value pairs
      */
-    router.put('/:id', (req, res, next) => {
+    router.put('/:id', extractMiddleware(before, 'put', '/:id'), (req, res, next) => {
         try {
             Resource.findByIdAndUpdate(req.params.id, {$set: req.body}, {
                 new: true,
@@ -138,61 +182,50 @@ const buildRoutes = (Resource, Extensions = []) => {
             })
                 .then((data) => {
                     res.send(data);
+                    next();
                 })
                 .catch(e => next(Err(400, e.message)));
         }
         catch (e) {
             next(e);
         }
-    });
+    }, extractMiddleware(after, 'put', '/:id'));
 
     /**
      * Creates a new resource object
      */
-    router.post('/', (req, res, next) => {
+    router.post('/', extractMiddleware(before, 'post', '/'), (req, res, next) => {
         try {
             let resource = new Resource(req.body);
             resource.save()
                 .then((resource) => {
                     res.send(resource);
+                    next();
                 })
                 .catch(e => next(Err(400, e.message)));
         }
         catch (e) {
             next(e);
         }
-    });
+    }, extractMiddleware(after, 'post', '/'));
 
     /**
      * Deletes a resource by id
      */
-    router.delete('/:id', (req, res, next) => {
+    router.delete('/:id', extractMiddleware(before, 'delete', '/:id'), (req, res, next) => {
         try {
             Resource.find({_id: req.params.id}).remove().exec()
                 .then((data) => {
                     res.send(data);
+                    next();
                 });
         }
         catch (e) {
             next(e);
         }
-    });
+    }, extractMiddleware(before, 'delete', '/:id'));
 
     return router;
-};
-
-/**
- * Used for creating custom routes or overwriting the default ones
-* @param method : String    - Accepted values are [get, post, put, delete]
-* @param route  : String    
-* @param func   : Function
-*/
-const Route = (method, route, func) => {
-    return {
-        method: method,
-        route: route,
-        function: func
-    }
 };
 
 module.exports = {buildRoutes, Route};
